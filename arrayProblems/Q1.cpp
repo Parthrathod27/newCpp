8ec6b3d8b9cfe4e1fda62dab5b996de6e18d8239
Parthrathod27 <parthvrathod27@gmail.com>
2025-08-27 20:31:47 +0530

    two sum Problem

--- arrayProblems/Q1.cpp
+++ arrayProblems/Q1.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
-using namespace std;
+                           //<<<<<TWO SUM >>>>>
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.*/
+
+
+/*Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].*/
+
+
+//psudo code:
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int t){
+    for(int i=0;i<nums.size();i++){
+        for(int j=i+1;j<nums.size();j++){
+            if(nums[i]+nums[j]==target){
+                return [i,j];
+            }
+        }
+    }
+    return {}
+ }
+};
