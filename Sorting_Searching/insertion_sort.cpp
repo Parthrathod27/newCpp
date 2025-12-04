97321c08865cabc2a0ddaddff42a3245469db7ce
Parthrathod27 <parthvrathod27@gmail.com>
2025-12-04 19:33:35 +0530

    insertion sort

---
+++ Sorting_Searching/insertion_sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[],int n){
+  
+   for(int i=1;i<n;i++){
+     
+    int curr=arr[i];
+    int prev=i-1;
+
+   while(prev>=0 && arr[prev]>curr){
+       
+      arr[prev+1]=arr[prev];
+      prev--;
+    }
+   arr[prev+1]=curr;
+  }
+}
+
+
+int main() {
+    int arr[5];
+
+    cout << "Enter 5 numbers: ";
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i]; 
+    }
+
+    insertionSort(arr, 5);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
